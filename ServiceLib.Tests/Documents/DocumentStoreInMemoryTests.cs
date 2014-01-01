accc1823539381497b0e51bdeb21bb9cd379e40b
Milan Wilczak <wlkodlak@seznam.cz>
2014-01-01 11:46:09 +0100

    Memory document store tests

---
+++ ServiceLib.Tests/Documents/DocumentStoreInMemoryTests.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLib.Tests.Documents
+{
+    [TestClass]
+    public class DocumentStoreInMemoryTests
+    {
+        private IDocumentStore _store;
+        private TestExecutor _executor;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _executor = new TestExecutor();
+            _store = CreateEmptyDocumentStore(_executor);
+        }
+
+        [TestMethod]
+        public void GettingNonexistentDocument()
+        {
+            VerifyDocument("testing", "document", DocumentStoreVersion.New, null);
+        }
+
+        [TestMethod]
+        public void CreatingNewDocument()
+        {
+            SaveDocument("testing", "document", DocumentStoreVersion.New, "new contents", "saved");
+            VerifyDocument("testing", "document", DocumentStoreVersion.At(1), "new contents");
+        }
+
+        [TestMethod]
+        public void ConflictWhenSavingNew()
+        {
+            SetupDocument("testing", "document", "original");
+            SaveDocument("testing", "document", DocumentStoreVersion.New, "new contents", "conflict");
+            VerifyDocument("testing", "document", DocumentStoreVersion.At(1), "original");
+        }
+
+        [TestMethod]
+        public void DeleteAll()
+        {
+            SetupDocument("testing", "document", "original");
+            DeleteAll("testing");
+            VerifyDocument("testing", "document", DocumentStoreVersion.New, null);
+        }
+
+        [TestMethod]
+        public void WatchChanges_SameDocument()
+        {
+            SetupDocument("testing", "document", "original");
+            int changes = 0;
+            _store.SubFolder("testing").WatchChanges("document", () => changes++);
+            SaveDocument("testing", "document", DocumentStoreVersion.Any, "new contents", "saved");
+            Assert.AreEqual(1, changes, "Changes count");
+        }
+
+        [TestMethod]
+        public void WatchChanges_DifferentDocument()
+        {
+            SetupDocument("testing", "document1", "A");
+            SetupDocument("testing", "document2", "B");
+            int changes = 0;
+            _store.SubFolder("testing").WatchChanges("document1", () => changes++);
+            SaveDocument("testing", "document2", DocumentStoreVersion.Any, "new contents", "saved");
+            Assert.AreEqual(0, changes, "Changes count");
+        }
+
+        protected void SetupDocument(string folder, string name, string contents)
+        {
+            _store.SubFolder(folder).SaveDocument(name, contents, DocumentStoreVersion.Any,
+                () => { }, () => { }, ex => { });
+            _executor.Process();
+        }
+
+        protected void VerifyDocument(string folder, string name, DocumentStoreVersion expectedVersion, string expectedContents)
+        {
+            string contents = "";
+            int version = 0;
+            bool failed = false;
+            _store.SubFolder(folder).GetDocument(name, (v, c) => { version = v; contents = c; }, () => { }, ex => failed = true);
+            _executor.Process();
+            Assert.IsFalse(failed, "GetDocument({0}) failed", name);
+            if (!expectedVersion.VerifyVersion(version))
+                Assert.AreEqual(expectedVersion, DocumentStoreVersion.At(version), "Unexpected version for {0}", name);
+            Assert.AreEqual(expectedContents ?? "", contents, "Contents for {0}", name);
+        }
+
+        protected void DeleteAll(string folder)
+        {
+            _store.SubFolder(folder).DeleteAll(() => { }, ex => { });
+            _executor.Process();
+        }
+
+        protected void SaveDocument(string folder, string name, DocumentStoreVersion expectedVersion, string contents, string expectedOutcome)
+        {
+            string resultSave = null;
+            _store.SubFolder(folder).SaveDocument(name, contents, expectedVersion, 
+                () => resultSave = "saved", () => resultSave = "conflict", ex => resultSave = "error");
+            _executor.Process();
+            Assert.AreEqual(expectedOutcome, resultSave, "Outcome for {0}", name);
+        }
+
+
+        protected virtual IDocumentStore CreateEmptyDocumentStore(IQueueExecution executor)
+        {
+            return new DocumentStoreInMemory(executor);
+        }
+
+        protected class TestExecutor : IQueueExecution
+        {
+            private Queue<IQueuedExecutionDispatcher> _queue = new Queue<IQueuedExecutionDispatcher>();
+
+            public void Enqueue(IQueuedExecutionDispatcher handler)
+            {
+                _queue.Enqueue(handler);
+            }
+
+            public void Process()
+            {
+                while (_queue.Count > 0)
+                    _queue.Dequeue().Execute();
+            }
+        }
+    }
+}
---
+++ ServiceLib.Tests/Documents/DocumentStoreVersionTests.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ServiceLib.Tests.Documents
+{
+    [TestClass]
+    public class DocumentStoreVersionTests
+    {
+        [TestMethod]
+        public void Equality()
+        {
+            Assert.AreEqual(DocumentStoreVersion.Any, DocumentStoreVersion.Any);
+            Assert.AreEqual(DocumentStoreVersion.New, DocumentStoreVersion.New);
+            Assert.AreEqual(DocumentStoreVersion.At(1), DocumentStoreVersion.At(1));
+            Assert.AreEqual(DocumentStoreVersion.At(2), DocumentStoreVersion.At(2));
+            Assert.AreEqual(DocumentStoreVersion.New, DocumentStoreVersion.At(0));
+            Assert.AreNotEqual(DocumentStoreVersion.Any, DocumentStoreVersion.New);
+            Assert.AreNotEqual(DocumentStoreVersion.Any, DocumentStoreVersion.At(0));
+            Assert.AreNotEqual(DocumentStoreVersion.Any, DocumentStoreVersion.At(1));
+            Assert.AreNotEqual(DocumentStoreVersion.New, DocumentStoreVersion.Any);
+            Assert.AreNotEqual(DocumentStoreVersion.New, DocumentStoreVersion.At(1));
+            Assert.AreNotEqual(DocumentStoreVersion.At(2), DocumentStoreVersion.At(1));
+        }
+
+        [TestMethod]
+        public void ConvertToString()
+        {
+            Assert.AreEqual("New", DocumentStoreVersion.New.ToString());
+            Assert.AreEqual("Any", DocumentStoreVersion.Any.ToString());
+            Assert.AreEqual("New", DocumentStoreVersion.At(0).ToString());
+            Assert.AreEqual("Version 47", DocumentStoreVersion.At(47).ToString());
+        }
+
+        [TestMethod]
+        public void AllowNew()
+        {
+            Assert.IsTrue(DocumentStoreVersion.Any.AllowNew, "Any");
+            Assert.IsTrue(DocumentStoreVersion.New.AllowNew, "New");
+            Assert.IsTrue(DocumentStoreVersion.At(0).AllowNew, "At(0)");
+            Assert.IsFalse(DocumentStoreVersion.At(4).AllowNew, "At(4)");
+        }
+
+        [TestMethod]
+        public void VerifyVersion_Any()
+        {
+            Assert.IsTrue(DocumentStoreVersion.Any.VerifyVersion(0), "0");
+            Assert.IsTrue(DocumentStoreVersion.Any.VerifyVersion(5), "5");
+        }
+
+        [TestMethod]
+        public void VerifyVersion_New()
+        {
+            Assert.IsTrue(DocumentStoreVersion.New.VerifyVersion(0), "0");
+            Assert.IsFalse(DocumentStoreVersion.New.VerifyVersion(5), "5");
+        }
+
+        [TestMethod]
+        public void VerifyVersion_At0()
+        {
+            Assert.IsTrue(DocumentStoreVersion.At(0).VerifyVersion(0), "0");
+            Assert.IsFalse(DocumentStoreVersion.At(0).VerifyVersion(5), "5");
+        }
+
+        [TestMethod]
+        public void VerifyVersion_AtVersion()
+        {
+            Assert.IsFalse(DocumentStoreVersion.At(5).VerifyVersion(0), "0");
+            Assert.IsFalse(DocumentStoreVersion.At(5).VerifyVersion(3), "3");
+            Assert.IsTrue(DocumentStoreVersion.At(5).VerifyVersion(5), "5");
+            Assert.IsFalse(DocumentStoreVersion.At(5).VerifyVersion(8), "8");
+        }
+    }
+}
