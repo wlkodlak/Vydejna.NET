251e99040b5d3c4eab6e808c580f9c07ffb42e0f
wlkodlak <milan.wilczak@gmail.com>
2014-02-08 22:48:51 +0100

    SkupinaNecislovanehoNaradi

---
+++ Vydejna.Domain.Tests/NecislovaneNaradiTesty/SkupinaNecislovanehoNaradiTest.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Vydejna.Domain.Tests.NecislovaneNaradiTesty
+{
+    [TestClass]
+    public class SkupinaNecislovanehoNaradiTest
+    {
+        [TestMethod]
+        public void KonstrukceZeSoucasti()
+        {
+            var datum = new DateTime(2013, 8, 13, 22, 43, 33);
+            var skupina = new SkupinaNecislovanehoNaradi(datum, 30m, CerstvostNecislovanehoNaradi.Nove, 12);
+            Assert.AreEqual(datum, skupina.DatumCerstvosti, "DatumCerstvosti");
+            Assert.AreEqual(30m, skupina.Cena, "Cena");
+            Assert.AreEqual(CerstvostNecislovanehoNaradi.Nove, skupina.Cerstvost, "Cerstvost");
+            Assert.AreEqual(12, skupina.Pocet, "Pocet");
+        }
+
+        [TestMethod]
+        public void Equality()
+        {
+            var a = new SkupinaNecislovanehoNaradi(new DateTime(2013, 4, 12), 10m, CerstvostNecislovanehoNaradi.Nove, 12);
+            var b = new SkupinaNecislovanehoNaradi(new DateTime(2013, 4, 12), 10m, CerstvostNecislovanehoNaradi.Nove, 12);
+            var c = new SkupinaNecislovanehoNaradi(new DateTime(2013, 4, 12), 10m, CerstvostNecislovanehoNaradi.Nove, 8);
+            var d = new SkupinaNecislovanehoNaradi(new DateTime(2013, 4, 12), 10m, CerstvostNecislovanehoNaradi.Opravene, 12);
+            var e = new SkupinaNecislovanehoNaradi(new DateTime(2013, 4, 12), 10m, CerstvostNecislovanehoNaradi.Opravene, 12);
+            var f = new SkupinaNecislovanehoNaradi(new DateTime(2013, 4, 12), 5m, CerstvostNecislovanehoNaradi.Opravene, 12);
+            var g = new SkupinaNecislovanehoNaradi(new DateTime(2013, 4, 10), 10m, CerstvostNecislovanehoNaradi.Opravene, 12);
+            Assert.AreEqual(a, b);
+            Assert.AreEqual(d, e);
+            Assert.AreNotEqual(b, c);
+            Assert.AreNotEqual(a, d);
+            Assert.AreNotEqual(d, f);
+            Assert.AreNotEqual(d, g);
+        }
+
+        [TestMethod]
+        public void PricitaniZvysujePocet()
+        {
+            var original = new SkupinaNecislovanehoNaradi(new DateTime(2013, 4, 12), 10m, CerstvostNecislovanehoNaradi.Nove, 12);
+            var expected = new SkupinaNecislovanehoNaradi(new DateTime(2013, 4, 12), 10m, CerstvostNecislovanehoNaradi.Nove, 18);
+            var actual = original.Pridat(6);
+            Assert.AreEqual(expected, actual, "Po pricteni");
+        }
+
+        [TestMethod]
+        public void PricitaniNemeniPuvodniInstanci()
+        {
+            var original = new SkupinaNecislovanehoNaradi(new DateTime(2013, 4, 12), 10m, CerstvostNecislovanehoNaradi.Nove, 12);
+            var before = new SkupinaNecislovanehoNaradi(new DateTime(2013, 4, 12), 10m, CerstvostNecislovanehoNaradi.Nove, 12);
+            before.Pridat(6);
+            Assert.AreEqual(before, original, "Zakazana modifikace");
+        }
+
+        [TestMethod]
+        public void OdecitaniSnizujePocet()
+        {
+            var original = new SkupinaNecislovanehoNaradi(new DateTime(2013, 4, 12), 10m, CerstvostNecislovanehoNaradi.Nove, 12);
+            var expected = new SkupinaNecislovanehoNaradi(new DateTime(2013, 4, 12), 10m, CerstvostNecislovanehoNaradi.Nove, 8);
+            var actual = original.Odebrat(4);
+            Assert.AreEqual(expected, actual, "Po odecteni");
+        }
+
+        [TestMethod]
+        public void OdecitaniNejdePodNulu()
+        {
+            var original = new SkupinaNecislovanehoNaradi(new DateTime(2013, 4, 12), 10m, CerstvostNecislovanehoNaradi.Nove, 3);
+            var expected = new SkupinaNecislovanehoNaradi(new DateTime(2013, 4, 12), 10m, CerstvostNecislovanehoNaradi.Nove, 0);
+            var actual = original.Odebrat(4);
+            Assert.AreEqual(expected, actual, "Po odecteni");
+        }
+
+        [TestMethod]
+        public void OdecitaniNemeniObjekt()
+        {
+            var original = new SkupinaNecislovanehoNaradi(new DateTime(2013, 4, 12), 10m, CerstvostNecislovanehoNaradi.Nove, 12);
+            var before = new SkupinaNecislovanehoNaradi(new DateTime(2013, 4, 12), 10m, CerstvostNecislovanehoNaradi.Nove, 12);
+            before.Odebrat(4);
+            Assert.AreEqual(before, original, "Zakazana modifikace");
+        }
+
+        [TestMethod]
+        public void ShodaNezohlednujePocet()
+        {
+            var a = new SkupinaNecislovanehoNaradi(new DateTime(2013, 4, 12), 10m, CerstvostNecislovanehoNaradi.Nove, 12);
+            var b = new SkupinaNecislovanehoNaradi(new DateTime(2013, 4, 12), 10m, CerstvostNecislovanehoNaradi.Nove, 12);
+            var c = new SkupinaNecislovanehoNaradi(new DateTime(2013, 4, 12), 10m, CerstvostNecislovanehoNaradi.Nove, 8);
+            var d = new SkupinaNecislovanehoNaradi(new DateTime(2013, 4, 12), 10m, CerstvostNecislovanehoNaradi.Opravene, 12);
+            var e = new SkupinaNecislovanehoNaradi(new DateTime(2013, 4, 12), 10m, CerstvostNecislovanehoNaradi.Opravene, 12);
+            var f = new SkupinaNecislovanehoNaradi(new DateTime(2013, 4, 12), 5m, CerstvostNecislovanehoNaradi.Opravene, 12);
+            var g = new SkupinaNecislovanehoNaradi(new DateTime(2013, 4, 10), 10m, CerstvostNecislovanehoNaradi.Opravene, 12);
+            Assert.IsTrue(a.Odpovida(b), "{0} ~ {1}");
+            Assert.IsTrue(a.Odpovida(c), "{0} ~ {1}");
+            Assert.IsTrue(!a.Odpovida(d), "{0} !~ {1}");
+            Assert.IsTrue(d.Odpovida(e), "{0} ~ {1}");
+            Assert.IsTrue(!d.Odpovida(f), "{0} !~ {1}");
+            Assert.IsTrue(!d.Odpovida(g), "{0} !~ {1}");
+        }
+    }
+}
---
+++ Vydejna.Domain/SkupinaNecislovanehoNaradi.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vydejna.Domain
+{
+    public enum CerstvostNecislovanehoNaradi
+    {
+        Nove, 
+        Opravene,
+        Pouzite
+    }
+
+    public class SkupinaNecislovanehoNaradi
+    {
+        public SkupinaNecislovanehoNaradi(DateTime datum, decimal cena, CerstvostNecislovanehoNaradi cerstvost, int pocet)
+        {
+            DatumCerstvosti = datum;
+            Cena = cena;
+            Cerstvost = cerstvost;
+            Pocet = pocet;
+        }
+
+        public DateTime DatumCerstvosti { get; private set; }
+        public decimal Cena { get; private set; }
+        public CerstvostNecislovanehoNaradi Cerstvost { get; private set; }
+        public int Pocet { get; private set; }
+
+        public SkupinaNecislovanehoNaradi Pridat(int pocet)
+        {
+            return new SkupinaNecislovanehoNaradi(DatumCerstvosti, Cena, Cerstvost, Pocet + pocet);
+        }
+
+        public SkupinaNecislovanehoNaradi Odebrat(int pocet)
+        {
+            return new SkupinaNecislovanehoNaradi(DatumCerstvosti, Cena, Cerstvost, Math.Max(Pocet - pocet, 0));
+        }
+
+        public bool Odpovida(SkupinaNecislovanehoNaradi oth)
+        {
+            return oth != null && DatumCerstvosti == oth.DatumCerstvosti && Cena == oth.Cena && Cerstvost == oth.Cerstvost;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var oth = obj as SkupinaNecislovanehoNaradi;
+            return Odpovida(oth) && Pocet == oth.Pocet;
+        }
+
+        public override int GetHashCode()
+        {
+            return DatumCerstvosti.GetHashCode() ^ Cena.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{{ datum: '{0:yyyy-MM-dd hh:mm:ss}', cena: {1}, cerstvost: {2}, pocet: {3} }}", DatumCerstvosti, Cena, Cerstvost, Pocet);
+        }
+    }
+}
