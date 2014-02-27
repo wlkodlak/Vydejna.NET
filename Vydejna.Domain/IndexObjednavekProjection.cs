389f436ecabb9641e85005d53434e2fa7a2e7d99
Milan Wilczak <milan.wilczak@gmail.com>
2014-02-27 23:00:52 +0100

    Priprava indexu objednavek a dodacich listu

---
+++ Vydejna.Domain/IndexObjednavekProjection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceLib;
+using ServiceStack.Text;
+using Vydejna.Contracts;
+
+namespace Vydejna.Domain
+{
+    public class IndexObjednavekProjection
+        : IEventProjection
+        , IHandle<CommandExecution<CislovaneNaradiPredanoKOpraveEvent>>
+        , IHandle<CommandExecution<NecislovaneNaradiPredanoKOpraveEvent>>
+        , IHandle<CommandExecution<CislovaneNaradiPrijatoZOpravyEvent>>
+        , IHandle<CommandExecution<NecislovaneNaradiPrijatoZOpravyEvent>>
+        , IHandle<CommandExecution<DefinovanDodavatelEvent>>
+    {
+    }
+
+    public class IndexObjednavekSerializer
+    {
+
+    }
+
+
+
+    public class IndexObjednavekReader
+        : IAnswer<NajitObjednavkuRequest, NajitObjednavkuResponse>
+        , IAnswer<NajitDodaciListRequest, NajitDodaciListResponse>
+    {
+
+    }
+}
