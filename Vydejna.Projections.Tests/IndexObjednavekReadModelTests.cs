9d2243374f7b5d3003086600e4435b26aac235e2
Milan Wilczak <milan.wilczak@gmail.com>
2014-03-24 22:37:59 +0100

    Jeste jedna projekce (text testu)

--- Vydejna.Projections.Tests/IndexObjednavekReadModelTests.cs
+++ Vydejna.Projections.Tests/IndexObjednavekReadModelTests.cs
@@ -8,5 +8,25 @@ namespace Vydejna.Projections.Tests
 {
     class IndexObjednavekReadModelTests
     {
+        /*
+         * Neexistujici cislo objednavky
+         * - objednavka nenalezena
+         * - cislo podle requestu
+         * - prazdny seznam kandidatu
+         * Existujici objednavka
+         * - objednavka nalezena vcetne cisla
+         * - kandidat obsahuje informace o cislu objednavky, kod a nazev dodavatele a termin dodani
+         * Objednavka muze mit vice kandidatu
+         * 
+         * Neexistujici cislo dodaciho listu
+         * - dodaci list nenalezen
+         * - cislo podle requestu
+         * - prazdny seznam kadidatu
+         * Existujici dodaci list
+         * - dodaci list nalezen vcetne cisla
+         * - kandidat obsahuje informace o cislu dodaciho listu, kod a nazev dodavatele
+         * - kandidat obsahuje seznam cisel objednavek, ke kterym je naradi dodano
+         * Kandidatu pro dodaci list muze byt vice
+         */
     }
 }
