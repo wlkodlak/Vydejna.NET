abc92c7a7119b845f479392db1cbe5eac422ced4
Milan Wilczak <milan.wilczak@gmail.com>
2014-03-28 01:27:28 +0100

    Work in progress

--- Vydejna.Projections.Tests/SeznamNaradiReadModelTests.cs
+++ Vydejna.Projections.Tests/SeznamNaradiReadModelTests.cs
@@ -1,13 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServiceLib;
+using ServiceLib.Tests.TestUtils;
+using System;
+using Vydejna.Contracts;
+using Vydejna.Projections.SeznamNaradiReadModel;
 
 namespace Vydejna.Projections.Tests
 {
-    class SeznamNaradiReadModelTests
+    [TestClass]
+    public class SeznamNaradiReadModelTests : ReadModelTestBase
     {
+        private ZiskatSeznamNaradiResponse _response;
+
+        [TestMethod]
+        public void NeexistujiciDataDavajiPrazdnyVysledek()
+        {
+            ZiskatStranku();
+            Assert.AreEqual(0, _response.PocetCelkem, "PocetCelkem");
+            Assert.AreEqual(0, _response.PocetStranek, "PocetStranek");
+            Assert.AreEqual(1, _response.Stranka, "Stranka");
+            Assert.IsNotNull(_response.SeznamNaradi, "SeznamNaradi");
+            Assert.AreEqual(0, _response.SeznamNaradi.Count, "SeznamNaradi.Count");
+        }
+
+        [TestMethod]
+        public void DefinovaneNaradiMaVlastnostiPodleUdalosti()
+        {
+            var definiceNaradi = new DefinovanoNaradiEvent
+            {
+                NaradiId = Guid.NewGuid(),
+                Verze = 1,
+                Vykres = "888-809",
+                Rozmer = "prum.36 č.5",
+                Druh = "opěrka"
+            };
+            SendEvent(definiceNaradi);
+            ZiskatStranku();
+            Assert.AreNotEqual(0, _response.SeznamNaradi.Count, "SeznamNaradi.Count");
+            var naradi = _response.SeznamNaradi[0];
+            Assert.AreEqual(definiceNaradi.NaradiId, naradi.Id, "Id");
+            Assert.AreEqual(definiceNaradi.Vykres, naradi.Vykres, "Vykres");
+            Assert.AreEqual(definiceNaradi.Rozmer, naradi.Rozmer, "Rozmer");
+            Assert.AreEqual(definiceNaradi.Druh, naradi.Druh, "Druh");
+            Assert.AreEqual(true, naradi.Aktivni, "Aktivni");
+        }
+
         /*
          * Neexistujici data davaji prazdny vysledek
          * Definovane naradi ma vlastnosti podle udalosti
@@ -18,5 +55,20 @@ namespace Vydejna.Projections.Tests
          * Metadata pri velkem mnozstvi naradi
          * Druha stranka serazeneho seznamu
          */
+
+        private void ZiskatStranku(int stranka = 1)
+        {
+            _response = ReadProjection<ZiskatSeznamNaradiRequest, ZiskatSeznamNaradiResponse>(new ZiskatSeznamNaradiRequest(stranka));
+        }
+
+        protected override IEventProjection CreateProjection()
+        {
+            return new SeznamNaradiProjection(new SeznamNaradiRepository(_folder), _executor, _time);
+        }
+
+        protected override object CreateReader()
+        {
+            return new SeznamNaradiReader(new SeznamNaradiRepository(_folder), _executor, _time);
+        }
     }
 }
